e7d5a939bab6f50197d474886119ab40179164d6
bl903068 <bl903068@etu.unice.fr>
2012-11-09 03:33:07 -0800

    cours 8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
-class SessionsController < ApplicationController
+class SessionController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.find_by_email(params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			sign_in user
+			redirect_to user
+			# Sign the user in and redirect to the user's show page.
+		else
+			flash[:error] = 'Invalid email/password combination' # Not quite rigth!
+			render 'new'
+		end
+	end
+
+	def destroy
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 FirstApp::Application.routes.draw do
+	resources :users
+	resources :sessions, only: [:new, :create, :destroy]
+
+	match '/signup', to: 'users#new'
+	match '/signin', to: 'sessions#new'
+	match '/signout', to: 'sessions#destroy', via: :delete
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +62,5 @@ FirstApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
 end
